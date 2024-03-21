9b76e1877a3aa809b66ae2e81f568eef96b47bd7
Nate Maninger <me@n8m.us>
2024-03-20 17:25:02 -0700

    separate chart bundle

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+	rollupOptions: {
+		output: {
+			manualChunks: {
+				'charts': ['apexcharts','vue3-apexcharts']
+			}
+		}
+	}
   }
 })
