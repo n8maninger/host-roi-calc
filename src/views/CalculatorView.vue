5b8dca8972b7d8f38db84e815740d2ee3a8016dd
Nate Maninger <me@n8m.us>
2024-03-20 23:14:44 -0700

    better labels

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -3,7 +3,9 @@ import { onMounted, ref, watch } from 'vue'
 import VueApexCharts from 'vue3-apexcharts'
 import CurrencyInput from '@/components/CurrencyInput.vue'
 
-const years = ref(10),
+const maxMonths = 10 * 12;
+
+const chart = ref(null), // chart component
 	currency=ref('usd'),
 	// costs
 	initialCost = ref(400.00),
@@ -20,7 +22,7 @@ const years = ref(10),
 	egressUsage = ref(0.00),
 
 	chartSeries = ref([{ data: [] }, { data: [] }]),
-	chartOpts = {
+	chartOpts = ref({
 		chart: {
 			height: '100%',
 			width: '100%',
@@ -42,11 +44,23 @@ const years = ref(10),
 				}
 			}
 		},
-		xaxis: {
-			type: 'numeric',
-			labels: {
+		tooltip: {
+			x: {
 				formatter: function (value) {
 					return `Month ${Math.floor(value)}`;
+				}
+			},
+		},
+		xaxis: {
+			type: 'numeric',
+			tickAmount: 'dataPoints',
+			stepSize: 1,
+			labels: {
+				formatter: function (value) {
+					console.log(value, isNaN(value))
+					if (!isNaN(value) && value % 12 === 0)
+						return `Year ${Math.floor(value/12)}`;
+					return '';
 				}
 			}
 		},
@@ -57,7 +71,7 @@ const years = ref(10),
 				}
 			}
 		}
-	};
+	});
 
 function generateChart() {
 	let storageUsage = 0,
@@ -67,11 +81,6 @@ function generateChart() {
 
 	const series = [
 			{
-				name: 'Revenue',
-				data: [],
-				color: '#6bc37d'
-			},
-			{
 				name: 'Risked Collateral',
 				data: [],
 				color: '#f6e05e'
@@ -80,11 +89,18 @@ function generateChart() {
 				name: 'Cost',
 				data: [],
 				color: '#be5b5b'
+			},
+			{
+				name: 'Revenue',
+				data: [],
+				color: '#6bc37d'
 			}
+			
 		],
 		labels = [];
 
-	for (let i = 0; i < years.value*12; i++) {
+	let breakEven = -1;
+	for (let i = 0; i < maxMonths-1; i++) {
 		let ingress = 0;
 		if (storageUsage < totalStorage.value) {
 			let ramp = 0.2 * i;
@@ -94,31 +110,58 @@ function generateChart() {
 			storageUsage += ingress;
 		}
 
+		if (breakEven > 0 && i >= breakEven*2)
+			break;
+
 		totalRevenue += (storageUsage * storagePricing.value) + (ingress * ingressPricing.value) + (egressUsage.value * egressPricing.value),
 			totalCollateral += (ingress * collateralRatio.value * storagePricing.value),
 			totalCost += maintenanceCost.value;
 
-		series[0].data.push(totalRevenue)
-		series[1].data.push(totalCollateral);
-		series[2].data.push(totalCost);
+		if (totalRevenue >= totalCost && breakEven === -1)
+			breakEven = i-1;
+
+		series[0].data.push(totalCollateral);
+		series[1].data.push(totalCost);
+		series[2].data.push(totalRevenue)
 		labels.push(i+1);
 	}
 	chartSeries.value = series;
-	chartOpts.labels = labels;
+	chartOpts.value.labels = labels;
+
+	if (!chartOpts.value.markers)
+		chartOpts.value.markers = { discrete: [] };
+	
+	if (breakEven === -1)
+		chartOpts.value.markers.discrete = [];
+	else {
+		chartOpts.value.markers.discrete = [
+			{
+				seriesIndex: 2,
+				dataPointIndex: breakEven,
+				shape: 'circle',
+				size: 6,
+				strokeColor: '#fff',
+				fillColor: '#6bc37d',
+				position: top
+			}
+		];
+	}
+
+	chart.value.updateOptions(chartOpts.value);
 }
 
 onMounted(async () => {
 	await generateChart();
 });
 
-watch([years, currency, initialCost, maintenanceCost, totalStorage, storagePricing, collateralRatio, ingressPricing, egressPricing, ingressUsage, egressUsage], generateChart);
+watch([currency, initialCost, maintenanceCost, totalStorage, storagePricing, collateralRatio, ingressPricing, egressPricing, ingressUsage, egressUsage], generateChart);
 </script>
 
 <template>
 	<main class="md:flex md:flex-col h-screen">
 		<div class="max-w-screen-md h-screen md:h-auto flex flex-col m-auto">
 			<div class="flex-1 md:flex-none md:h-96">
-				<VueApexCharts :options="chartOpts" height="100%" width="100%" :series="chartSeries" />
+				<VueApexCharts ref="chart" :options="chartOpts" height="100%" width="100%" :series="chartSeries" />
 			</div>
 			<form class="bg-zinc-300 p-2 md:rounded-md md:shadow-md">
 				<label for="currencySelect" class="block text-sm font-medium leading-6 text-gray-900">Currency</label>
@@ -150,19 +193,18 @@ watch([years, currency, initialCost, maintenanceCost, totalStorage, storagePrici
 					<div class="flex gap-2">
 						<div class="flex-1">
 							<label for="totalStorage" class="block text-sm font-medium leading-6 text-gray-900">Total Storage (TB)</label>
-							<CurrencyInput :currency="currency" id="totalStorage" type="text"
-							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-							placehold="0.00" v-model.number="totalStorage"/>
+							<input id="totalStorage" type="text" class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="totalStorage"/>
 						</div>
 						<div class="flex-1">
 								<label for="ingressUsage" class="block text-sm font-medium leading-6 text-gray-900">Ingress (TB/month)</label>
-							<CurrencyInput :currency="currency" id="ingressUsage" type="text"
+							<input id="ingressUsage" type="text"
 								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 								placehold="0.00" v-model.number="ingressUsage"/>
 						</div>
 						<div class="flex-1">
 							<label for="egressUsage" class="block text-sm font-medium leading-6 text-gray-900">Egress (TB/month)</label>
-							<CurrencyInput :currency="currency" id="egressUsage" type="text"
+							<input id="egressUsage" type="text"
 								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 								placehold="0.00" v-model.number="egressUsage"/>
 						</div>
