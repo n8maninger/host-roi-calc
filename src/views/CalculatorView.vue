b6cec54eefcba649158e624ffdc72224ec3f6d78
Nate Maninger <me@n8m.us>
2024-03-20 21:52:17 -0700

    better control layout

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -23,7 +23,12 @@ const years = ref(10),
 	chartSeries = ref([{ data: [] }, { data: [] }]),
 	chartOpts = {
 		chart: {
+			height: '100%',
+			width: '100%',
 			type: 'line',
+			toolbar: {
+				show: false
+			},
 			animations: {
 				enabled: true,
 				easing: 'easeinout',
@@ -112,87 +117,100 @@ watch([years, initialCost, electricityCost, internetCost, maintenanceCost, other
 </script>
 
 <template>
-	<main class="h-screen flex overflow-hidden">
-		<form class="max-h-full p-4 bg-slate-300 overflow-y-auto">
-			<div class="border-b border-gray-900/10 mb-4 pb-4 pt-4">
-				<h2 class="text-base font-semibold leading-7 text-gray-900">Costs</h2>
-
-				<label for="initialCost" class="block text-sm font-medium leading-6 text-gray-900">Initial</label>
-				<input id="initialCost" type="text"
-					class="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="initialCost">
-
-				<label for="electricityCost" class="block text-sm font-medium leading-6 text-gray-900">Electricity
-					(per month)</label>
-				<input id="electricityCost" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="electricityCost">
-
-				<label for="internetCost" class="block text-sm font-medium leading-6 text-gray-900">Internet (per
-					month)</label>
-				<input id="internetCost" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="internetCost">
-
-				<label for="maintenanceCost" class="block text-sm font-medium leading-6 text-gray-900">Maintenance
-					(per month)</label>
-				<input id="maintenanceCost" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="maintenanceCost">
-
-				<label for="otherCost" class="block text-sm font-medium leading-6 text-gray-900">Other (per
-					month)</label>
-				<input id="otherCost" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="otherCost">
+	<main class="md:flex md:flex-col h-screen">
+		<div class="max-w-screen-md h-screen md:h-auto flex flex-col m-auto">
+			<div class="flex-1 md:flex-none md:h-96">
+				<VueApexCharts :options="chartOpts" height="100%" width="100%" :series="chartSeries" />
 			</div>
-			<div class="border-b border-gray-900/10 mb-4 pb-4 pt-4">
-				<h2 class="text-base font-semibold leading-7 text-gray-900">Storage Node</h2>
-				<label for="totalStorage" class="block text-sm font-medium leading-6 text-gray-900">Total Storage
-					(TB)</label>
-				<input id="totalStorage" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="totalStorage">
-			</div>
-			<div class="border-b border-gray-900/10 mb-4 pb-4 pt-4">
+			<form class="bg-zinc-300 p-2 md:rounded-md md:shadow-md">
+				<div class="border-b border-gray-900/10 mb-4 pb-4 gap-2">
+					<h2 class="text-base font-semibold leading-7 text-gray-900">Costs</h2>
+					<div class="flex gap-2">
+						<div class="flex-1">
+							<label for="initialCost" class="block text-sm font-medium leading-6 text-gray-900">Initial</label>
+							<input id="initialCost" type="text"
+								class="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="initialCost">
+						</div>
+						<div class="flex-1">
+							<label for="electricityCost" class="block text-sm font-medium leading-6 text-gray-900">Electricity (per month)</label>
+							<input id="electricityCost" type="text"
+								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="electricityCost">
+						</div>
+					</div>
+					<div class="flex gap-2">
+						<div class="flex-1">
+							<label for="internetCost" class="block text-sm font-medium leading-6 text-gray-900">Internet (per month)</label>
+							<input id="internetCost" type="text"
+								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="internetCost">
+						</div>
+						<div class="flex-1">
+							<label for="maintenanceCost" class="block text-sm font-medium leading-6 text-gray-900">Maintenance (per month)</label>
+							<input id="maintenanceCost" type="text"
+								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="maintenanceCost">
+						</div>
+						<div class="flex-1">
+						<label for="otherCost" class="block text-sm font-medium leading-6 text-gray-900">Other (per month)</label>
+						<input id="otherCost" type="text"
+							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							placehold="0.00" v-model.number="otherCost">
+						</div>
+					</div>
+				</div>
+				<div class="border-b border-gray-900/10 mb-4 pb-4 gap-2">
+					<h2 class="text-base font-semibold leading-7 text-gray-900">Usage</h2>
+					<div class="flex gap-2">
+						<div class="flex-1">
+							<label for="totalStorage" class="block text-sm font-medium leading-6 text-gray-900">Total Storage (TB)</label>
+							<input id="totalStorage" type="text"
+							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							placehold="0.00" v-model.number="totalStorage">
+						</div>
+						<div class="flex-1">
+								<label for="ingressUsage" class="block text-sm font-medium leading-6 text-gray-900">Ingress (TB/month)</label>
+							<input id="ingressUsage" type="text"
+								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="ingressUsage">
+						</div>
+						<div class="flex-1">
+							<label for="egressUsage" class="block text-sm font-medium leading-6 text-gray-900">Egress (TB/month)</label>
+							<input id="egressUsage" type="text"
+								class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+								placehold="0.00" v-model.number="egressUsage">
+						</div>
+					</div>
+				</div>
 				<h2 class="text-base font-semibold leading-7 text-gray-900">Pricing</h2>
-
-				<label for="storagePrice" class="block text-sm font-medium leading-6 text-gray-900">Storage (TB/month)</label>
-				<input id="storagePrice" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="storagePricing">
-				
-				<label for="collateralRatio" class="block text-sm font-medium leading-6 text-gray-900">Collateral ratio</label>
-				<input id="collateralRatio" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="2.00" v-model.number="collateralRatio">
-
-				<label for="ingressPrice" class="block text-sm font-medium leading-6 text-gray-900">Ingress (per
-					TB)</label>
-				<input id="ingressPrice" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="ingressPricing">
-
-				<label for="egressPrice" class="block text-sm font-medium leading-6 text-gray-900">Egress (per
-					TB)</label>
-				<input id="egressPrice" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="egressPricing">
-			</div>
-			<div class="border-b border-gray-900/10 mb-4 pb-4 pt-4">
-				<h2 class="text-base font-semibold leading-7 text-gray-900">Usage</h2>
-
-				<label for="ingressUsage" class="block text-sm font-medium leading-6 text-gray-900">Ingress (TB/month)</label>
-				<input id="ingressUsage" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="ingressUsage">
-
-				<label for="egressUsage" class="block text-sm font-medium leading-6 text-gray-900">Egress (TB/month)</label>
-				<input id="egressUsage" type="text"
-					class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					placehold="0.00" v-model.number="egressUsage">
-			</div>
-		</form>
-		<VueApexCharts class="flex-1" width="100%" height="100%" :options="chartOpts" :series="chartSeries" />
+				<div class="flex gap-2">
+					<div class="flex-1">
+						<label for="storagePrice" class="block text-sm font-medium leading-6 text-gray-900">Storage (TB/month)</label>
+						<input id="storagePrice" type="text"
+							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							placehold="0.00" v-model.number="storagePricing">
+					</div>
+					<div class="flex-1">
+						<label for="collateralRatio" class="block text-sm font-medium leading-6 text-gray-900">Collateral ratio</label>
+						<input id="collateralRatio" type="text"
+							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							placehold="2.00" v-model.number="collateralRatio">
+					</div>
+					<div class="flex-1">
+						<label for="ingressPrice" class="block text-sm font-medium leading-6 text-gray-900">Ingress (per TB)</label>
+						<input id="ingressPrice" type="text"
+							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							placehold="0.00" v-model.number="ingressPricing">
+					</div>
+					<div class="flex-1">
+						<label for="egressPrice" class="block text-sm font-medium leading-6 text-gray-900">Egress (per TB)</label>
+						<input id="egressPrice" type="text"
+							class="form-input mt-1 mb-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+							placehold="0.00" v-model.number="egressPricing">
+					</div>
+				</div>
+			</form>
+		</div>
 	</main>
 </template>
